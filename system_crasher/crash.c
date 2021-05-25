4ea760abe4be57d2edba6068d0e0d1714a7b5e56
Magda Lipka <dev@magdalipka.com>
2021-05-25 20:14:30 +0200

    crash to hang system

--- system_crasher/crash.c
+++ system_crasher/crash.c
@@ -6,17 +6,26 @@
 MODULE_LICENSE("GPL");
 
 static int hello(void) {
-    printk(KERN_INFO "Hello world.\n");
-    while (true) {
-      int address;
-      get_random_bytes(&address, sizeof(address)); 
-      if (address < 0) address = -address;
-      memset(address, 0, 8);
-    }
-    return 0;
+  printk(KERN_INFO "Hello world.\n");
+
+  unsigned char* p;
+  unsigned int address;
+  unsigned int memsize;
+
+  address = 0x00000001;
+  memsize = 1024:
+
+  while (true) {
+
+    p = ioremap(address, memsize);
+    writeb('0', p);
+    address++;
+
+  }
+  return 0;
 }
 static void goodbye(void) {
-    printk(KERN_INFO "Goodbye world.\n");
+  printk(KERN_INFO "Goodbye world.\n");
 }
 
 module_init(hello);
